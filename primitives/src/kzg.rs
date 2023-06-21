73d6d5182f0afd2fd18ffd47e131d158a0429171
DarkingLee <xiuerdwy@gmail.com>
2023-06-22 06:45:18 +0800

    primitives of kzg

---
+++ primitives/src/kzg.rs
@@ -0,0 +1,106 @@
+// Copyright 2023 ZeroDAO
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+extern crate alloc;
+
+use super::kzg_data::{Blob, BlsScalar, KZGCommitment, KZGProof};
+
+use alloc::string::String;
+use alloc::vec::Vec;
+use kzg::{FFTSettings, KZGSettings,};
+use rust_kzg_blst::{
+	eip_4844::{
+		compute_blob_kzg_proof_rust, load_trusted_setup_filename_rust,
+		verify_blob_kzg_proof_batch_rust, verify_blob_kzg_proof_rust,
+	},
+	types::{
+		fft_settings::FsFFTSettings, fr::FsFr, g1::FsG1, kzg_settings::FsKZGSettings, poly::FsPoly,
+	},
+};
+
+const TRUSTED_SETUP_FILENAME: &str = "eth-public-parameters.bin";
+
+pub struct KZG {
+	settings: FsKZGSettings,
+}
+
+impl KZG {
+	pub fn new() -> Self {
+		Self { settings: load_trusted_setup_filename_rust(TRUSTED_SETUP_FILENAME) }
+	}
+
+	pub fn compute_proof(&self, poly: &FsPoly, point_index: usize) -> Result<KZGProof, String> {
+		let x = self.settings.get_expanded_roots_of_unity_at(point_index as usize);
+		self.settings.compute_proof_single(poly, &x).map(KZGProof)
+	}
+
+	// Domain knowledge should not be disclosed
+	pub fn commit(&self, poly: &FsPoly) -> Result<KZGCommitment, String> {
+		self.settings.commit_to_poly(&poly).map(KZGCommitment)
+	}
+
+	pub fn verify(
+		&self,
+		commitment: &KZGCommitment,
+		num_values: usize,
+		index: u32,
+		scalar: &BlsScalar,
+		proof: &KZGProof,
+	) -> Result<bool, String> {
+		let fft_settings = Self::new_fft_settings(num_values)?;
+		let x = fft_settings.get_expanded_roots_of_unity_at(index as usize);
+
+		self.settings.check_proof_single(&commitment, &proof, &x, scalar)
+	}
+
+	pub fn compute_blob_proof(
+		&self,
+		blob: &Blob,
+		commitment: &KZGCommitment,
+	) -> Result<KZGProof, String> {
+		compute_blob_kzg_proof_rust(&blob.data, commitment, &self.settings).map(KZGProof)
+	}
+
+	pub fn verify_blob_proof(
+		&self,
+		blob: &Blob,
+		commitment: &KZGCommitment,
+		proof: &KZGProof,
+	) -> Result<bool, String> {
+		verify_blob_kzg_proof_rust(&blob.data, &commitment, &proof, &self.settings)
+	}
+
+	pub fn verify_blobs_proof_batch(
+		&self,
+		commitments: &[KZGCommitment],
+		proofs: &[KZGProof],
+		blobs: &[Blob],
+	) -> Result<bool, String> {
+		let blobs_fs_fr: [Vec<FsFr>] = blobs.iter().map(|blob| blob.data).collect();
+		let commitments_fs_g1: Vec<FsG1> =
+			commitments.iter().map(|commitment| commitment.0).collect();
+		let proofs_fs_g1: Vec<FsG1> = proofs.iter().map(|proof| proof.0).collect();
+		verify_blob_kzg_proof_batch_rust(
+			&blobs_fs_fr,
+			&commitments_fs_g1,
+			&proofs_fs_g1,
+			&self.settings,
+		)
+	}
+
+	fn new_fft_settings(num_values: usize) -> Result<FsFFTSettings, String> {
+		FsFFTSettings::new(
+			num_values.checked_sub(1).expect("Checked to be not empty above; qed").ilog2() as usize,
+		)
+	}
+}
---
+++ primitives/src/kzg_data.rs
@@ -0,0 +1,176 @@
+// Copyright 2023 ZeroDAO
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this filepub(crate)  except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+//! Tools for KZG commitment scheme
+
+extern crate alloc;
+
+use alloc::{
+	string::{String, ToString},
+	vec::Vec,
+};
+use core::hash::{Hash, Hasher};
+use derive_more::{AsMut, AsRef, Deref, DerefMut, From, Into};
+use kzg::{Fr, G1};
+use parity_scale_codec::{Decode, Encode, EncodeLike, Input, MaxEncodedLen};
+use rust_kzg_blst::types::{fr::FsFr, g1::FsG1};
+use scale_info::{Type, TypeInfo};
+
+macro_rules! kzg_data_size {
+	($name:ident, $type:ty, $size:expr) => {
+		#[derive(
+			Debug, Default, Copy, Clone, PartialEq, Eq, Into, From, AsRef, AsMut, Deref, DerefMut,
+		)]
+		#[repr(transparent)]
+		pub struct $name(pub $type);
+		impl $name {
+			const SIZE: usize = $size;
+
+			#[inline]
+			pub fn to_bytes(&self) -> [u8; $size] {
+				self.to_bytes()
+			}
+
+			#[inline]
+			pub fn try_from_bytes(bytes: &[u8; $size]) -> Result<Self, String> {
+				Ok($name(<$type>::from_bytes(bytes)?))
+			}
+		}
+
+		impl Hash for $name {
+			fn hash<H: Hasher>(&self, state: &mut H) {
+				self.to_bytes().hash(state);
+			}
+		}
+
+		impl From<$name> for [u8; $size] {
+			#[inline]
+			fn from(kzd_data: $name) -> Self {
+				kzd_data.to_bytes()
+			}
+		}
+
+		impl From<&$name> for [u8; $size] {
+			#[inline]
+			fn from(kzd_data: &$name) -> Self {
+				kzd_data.to_bytes()
+			}
+		}
+
+		impl TryFrom<&[u8; $size]> for $name {
+			type Error = String;
+
+			#[inline]
+			fn try_from(bytes: &[u8; $size]) -> Result<Self, Self::Error> {
+				Self::try_from_bytes(bytes)
+			}
+		}
+
+		impl TryFrom<[u8; $size]> for $name {
+			type Error = String;
+
+			#[inline]
+			fn try_from(bytes: [u8; $size]) -> Result<Self, Self::Error> {
+				Self::try_from(&bytes)
+			}
+		}
+
+		impl Encode for $name {
+			#[inline]
+			fn size_hint(&self) -> usize {
+				$size
+			}
+
+			fn using_encoded<R, F: FnOnce(&[u8]) -> R>(&self, f: F) -> R {
+				f(&self.to_bytes())
+			}
+
+			#[inline]
+			fn encoded_size(&self) -> usize {
+				$size
+			}
+		}
+
+		impl EncodeLike for $name {}
+
+		impl MaxEncodedLen for $name {
+			#[inline]
+			fn max_encoded_len() -> usize {
+				$size
+			}
+		}
+
+		impl Decode for $name {
+			fn decode<I: Input>(input: &mut I) -> Result<Self, parity_scale_codec::Error> {
+				Self::try_from_bytes(&Decode::decode(input)?).map_err(|error| {
+					parity_scale_codec::Error::from("Failed to decode from bytes")
+						.chain(alloc::format!("{error:?}"))
+				})
+			}
+
+			#[inline]
+			fn encoded_fixed_size() -> Option<usize> {
+				Some($size)
+			}
+		}
+
+		impl TypeInfo for $name {
+			type Identity = Self;
+
+			fn type_info() -> Type {
+				Type::builder()
+					.path(scale_info::Path::new(stringify!($name), module_path!()))
+					.docs(&["Commitment to polynomial"])
+					.composite(scale_info::build::Fields::named().field(|f| {
+						f.ty::<[u8; $size]>().name(stringify!(inner)).type_name("G1Affine")
+					}))
+			}
+		}
+	};
+}
+
+kzg_data_size!(KZGCommitment, FsG1, 48);
+kzg_data_size!(KZGProof, FsG1, 48);
+kzg_data_size!(BlsScalar, FsFr, 32);
+
+pub const BYTES_PER_BLOB: usize = BYTES_PER_FIELD_ELEMENT * FIELD_ELEMENTS_PER_BLOB;
+pub const BYTES_PER_FIELD_ELEMENT: usize = 32;
+pub const FIELD_ELEMENTS_PER_BLOB: usize = 4;
+
+#[derive(Debug, Default, Clone, PartialEq, Eq, From, AsRef, AsMut, Deref, DerefMut)]
+pub struct Blob(pub Vec<FsFr>);
+
+impl Blob {
+	#[inline]
+	pub fn try_from_bytes(bytes: &[u8]) -> Result<Self, String> {
+		if bytes.len() != BYTES_PER_BLOB {
+			return Err(format!(
+				"Invalid byte length. Expected {} got {}",
+				BYTES_PER_BLOB,
+				bytes.len(),
+			));
+		}
+		let data_result: Result<Vec<FsFr>, String> = bytes
+			.chunks(BYTES_PER_FIELD_ELEMENT)
+			.map(|chunk| {
+				chunk
+					.try_into()
+					.map_err(|_| "Chunked into incorrect number of bytes".to_string())
+					.and_then(FsFr::from_bytes)
+			})
+			.collect();
+
+		data_result.map(Self)
+	}
+}
