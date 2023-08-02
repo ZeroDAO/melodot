e591aecf808604058223f22bae236c478a884686
DarkingLee <xiuerdwy@gmail.com>
2023-08-02 15:08:21 +0800

    Fixed bugs in docker_run.sh

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -7,30 +7,29 @@ echo "*** Melodot ***"
 cd $(dirname ${BASH_SOURCE[0]})/..
 
 docker-compose down --remove-orphans
-docker-compose run --rm --service-ports dev $@
 
 # Get the arguments passed to the script
-command=$1
+cmd=$1
 crate=$2
 
 # Check if the crate parameter is provided
 if [ -z "$crate" ]; then
-  echo "Please provide the crate name."
+  docker-compose run --rm --service-ports dev $@
 else
   # Define a function to execute different commands
   execute_command() {
     crate_name="$1"
     cd "crates/$crate_name"
-    case "$command" in
-      "test")
-        cargo test
-        ;;
+    case "$cmd" in
       "build")
         cargo build
         ;;
+      "test")
+        cargo test
+        ;;
       # You can add more command options here
       *)
-        echo "Unknown command: $command"
+        echo "Unknown command: $cmd"
         exit 1
         ;;
     esac
@@ -39,5 +38,5 @@ else
   # Call the function to execute the command
   execute_command "$crate"
 
-  read -p "Tests completed. Press Enter to exit..."
+  read -p "$cmd completed. Press Enter to exit..."
 fi
