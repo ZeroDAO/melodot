09dfcdc18e6358e50e8b695a278fae4145a982b1
DarkingLee <xiuerdwy@gmail.com>
2023-11-30 20:42:35 +0800

    feat: init pallet_farmers_fortune

--- crates/proof-of-space/src/lib.rs
+++ crates/proof-of-space/src/lib.rs
@@ -11,8 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// This line specifies that the crate should not use the Rust standard library
-// if the "std" feature is not enabled. This is common in no-std environments like embedded systems.
+
 #![cfg_attr(not(feature = "std"), no_std)]
 
 extern crate alloc;
--- crates/proof-of-space/src/segment_entity.rs
+++ crates/proof-of-space/src/segment_entity.rs
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 use crate::{get_record_id, DasKv, Encode, Decode, FarmerId, Segment, Table};
 use core::marker::PhantomData; // Used for type safety without storing actual data.
 use sp_runtime::traits::Hash as HashT; // Importing Hash trait for cryptographic operations.
